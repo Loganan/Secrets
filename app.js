2926372d8e448cedaf43ab5c1be93367043de4ac
Logan Walker <loganwalkernz@gmail.com>
2020-01-20 17:42:34 +1100

    Add hashing and salt using bcrypt.

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+//Removed md5 as chosen hashing tool
+//const md5 = require('md5'); //For hashing
+
+//Removed encryption in favour of hashing passwords
+//const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -23,7 +30,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
+//Removed encryption in favour of hashing passwords
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,35 +48,48 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if(err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  })
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+      //password: md5(req.body.password) //For hashing
+    });
+    newUser.save(function(err){
+      if(err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+});
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
   const password = req.body.password;
+  //const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err) {
       console.log(err);
     } else {
       if(foundUser) {
-        if(foundUser.password === password) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+    if(result === true){
+      //if password after hashing with the salt is equal to the hash in database
+      res.render("secrets");
+    }
+});
+
+        /*if(foundUser.password === password) {
           res.render("secrets");
-        }
+        }*/
       }
     }
   })
 });
+
 
 
 
