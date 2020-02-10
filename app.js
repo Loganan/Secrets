c5de8d0a2a5d8ae9249732731dd1a097fc947d32
Logan Walker <loganwalkernz@gmail.com>
2020-02-10 16:37:36 +1100

    Add OAuth2.0 google signup and login.

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+//Removes encryption in favour of passport
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 
 //Removed md5 as chosen hashing tool
 //const md5 = require('md5'); //For hashing
@@ -23,31 +30,152 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useUnifiedTopology: true, useNewUrlParser: true});
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 //Removed encryption in favour of hashing passwords
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
+passport.use(User.createStrategy());
+
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    //console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    //Successful authentication, redirect to secrets
+    res.redirect("/secrets");
+  });
+
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res){
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function(req, res) {
+
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
+
+app.post("/login", function(req, res) {
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on post 3000.");
+});
+
+//Removed this register/login code which used bcrypt. Commented for future reference.
+/*
+app.post("/register", function(req, res) {
 
   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     const newUser = new User({
@@ -55,55 +183,38 @@ app.post("/register", function(req, res){
       password: hash
       //password: md5(req.body.password) //For hashing
     });
-    newUser.save(function(err){
-      if(err) {
+    newUser.save(function(err) {
+      if (err) {
         console.log(err);
       } else {
         res.render("secrets");
       }
     });
-});
+  });
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
   //const password = md5(req.body.password);
 
-  User.findOne({email: username}, function(err, foundUser){
-    if(err) {
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
       console.log(err);
     } else {
-      if(foundUser) {
+      if (foundUser) {
         bcrypt.compare(password, foundUser.password, function(err, result) {
-    if(result === true){
-      //if password after hashing with the salt is equal to the hash in database
-      res.render("secrets");
-    }
-});
-
-        /*if(foundUser.password === password) {
-          res.render("secrets");
-        }*/
+          if (result === true) {
+            //if password after hashing with the salt is equal to the hash in database
+            res.render("secrets");
+          }
+        });
+        //if(foundUser.password === password) {
+        //  res.render("secrets");
+        //}
       }
     }
-  })
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function(){
-  console.log("Server started on post 3000.");
-});
+  });
+});*/
